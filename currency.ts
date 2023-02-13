8ce46b211ec63a96cb07d05dfe982fcefd2f64f2
areebahmed575 <areebahmed575@gmail.com>
2023-02-14 01:52:46 +0500

    add currency_converter

---
+++ currency.ts
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+
+let converter={
+    
+        "PKR":{
+            "USD":0.0044,
+            "GBP":0.0037,
+            "PKR":1
+
+        },
+        "GBP":{
+            "USD":1.21,
+            "PKR":271.79,
+            "GBP":1
+
+        },
+        "USD":{
+            "USD":1,
+            "PKR":225.50,
+            "GBP":0.83
+
+        } 
+    
+}
+type ansType={
+    from:"PKR"|"GBP"|"USD",
+    to:"PKR"|"GBP"|"USD",
+    amount:number
+}
+let answer:ansType=await inquirer.prompt([
+    {
+        type:"list",
+        name:"from",
+        choices:["PKR","GBP","USD"],
+        message:"Select your currency"
+    },
+
+    {
+        type:"list",
+        name:"to",
+        choices:["PKR","GBP","USD"],
+        message:"Select your convertion currency"
+    },
+
+    {
+        type:"number",
+        name:"amount",
+        message:"Enter convertion amount"
+    }
+
+])
+if(answer.from && answer.to && answer.amount){
+    let result=converter[answer.from][answer.to] * answer.amount
+    console.log("Your convertion from "+answer.from+" to "+answer.to+" "+result+" " )
+
+}
+else{
+    console.log("invalid inputs")
+} 
+
